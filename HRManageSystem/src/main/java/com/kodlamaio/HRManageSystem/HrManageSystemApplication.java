d06361ee4567b1f49eb373f1cbab0a363bcdd67b
meminb <mu.eminb_97@hotmail.com>
2021-05-16 13:04:56 +0300

    Database Design V.1

---
+++ HRManageSystem/src/main/java/com/kodlamaio/HRManageSystem/HrManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kodlamaio.HRManageSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManageSystemApplication.class, args);
+	}
+
+}
---
+++ HRManageSystem/src/test/java/com/kodlamaio/HRManageSystem/HrManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodlamaio.HRManageSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
